8b2fc8c272558a8f082da1b489b24935fad657f8
Ryan (Chenguang) Xu <xcg1234@gmail.com>
2021-05-03 21:15:18 -0400

    first

---
+++ components/Layout.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+const Layout = ({ children, title = 'Crypto Price Tracer' }) => {
+	return (
+		<div className='layout'>
+			<Head>
+				<title>{title}</title>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			<header className='header'></header>
+			<main>{children}</main>
+		</div>
+	);
+};
+export default Layout;
